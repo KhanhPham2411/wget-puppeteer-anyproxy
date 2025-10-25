a228bc3419f21e7248332e62f05ff348c0cd4be8
Khanh Pham <phamduckhanh2411@gmail.com>
2025-10-25 15:51:02 +0700

    Update wget_youtube.cmd to include a clean script execution before downloading content.

--- wget_youtube.cmd
+++ wget_youtube.cmd
@@ -9,7 +9,7 @@ REM common recursive config
 REM --mirror --no-parent
 REM --reject-regex "logout|language"
 
-wget -p -k --html-extension --no-check-certificate --load-cookies cookies.txt -P output -e use_proxy=on -e http_proxy=127.0.0.1:8001 -e https_proxy=127.0.0.1:8001 https://www.youtube.com/watch?v=0-q1KafFCLU
+npm run clean && wget -p -k --html-extension --no-check-certificate --load-cookies cookies.txt -P output -e use_proxy=on -e http_proxy=127.0.0.1:8001 -e https_proxy=127.0.0.1:8001 https://www.youtube.com/watch?v=0-q1KafFCLU
 
 
 
