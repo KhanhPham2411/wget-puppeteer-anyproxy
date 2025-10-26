fc36598bfc2ad1073419f10246d40901480cf885
Khanh Pham <phamduckhanh2411@gmail.com>
2025-10-26 10:27:38 +0700

    Update wget_sample.cmd to modify wget command for improved functionality; add rejection of JavaScript files and adjust URL for documentation retrieval.

--- wget_sample.cmd
+++ wget_sample.cmd
@@ -9,7 +9,9 @@ REM common recursive config
 REM --mirror --no-parent
 REM --reject-regex "logout|language"
 
-npm run clean && wget -p -k --html-extension --mirror --no-parent --no-check-certificate --load-cookies cookies.txt -P output -e use_proxy=on -e http_proxy=127.0.0.1:8001 -e https_proxy=127.0.0.1:8001 https://docs.developers.optimizely.com/feature-experimentation/
+REM npm run clean && wget -p -k --html-extension --mirror --no-parent --no-check-certificate --load-cookies cookies.txt -P output -e use_proxy=on -e http_proxy=127.0.0.1:8001 -e https_proxy=127.0.0.1:8001 https://docs.developers.optimizely.com/
+
+npm run clean && wget -p -k --html-extension --reject "*.js" --mirror --no-parent --no-check-certificate -P output https://docs.developers.optimizely.com/
 
 
 
